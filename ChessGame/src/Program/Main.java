9957f3e7c1fe0a43666e1243ca2739f58538c320
Grazielle Almeida <86306468+GaiaOcean@users.noreply.github.com>
2024-12-20 18:36:00 -0300

    Add files via upload

--- ChessGame/src/Program/Main.java
+++ ChessGame/src/Program/Main.java
@@ -1,8 +1,13 @@
+
 package Program;
+
+import BoardGame.Position;
 
 public class Main {
     public static void main(String[] args) {
+        Position pos = new Position(2,6);
 
+        System.out.println(pos);
 
     }
 }
